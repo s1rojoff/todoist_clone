c92db3b0a4b6402041be2e1f9d4f7463561e6e1e
shukhratov1ch <jorayevazik586@gmail.com>
2022-08-25 15:54:38 +0300

    created 6 sections in feature section

---
+++ src/components/features/components/featHeader/videoOverlay.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+<template>
+  <div
+    class="
+      w-full
+      h-[100vh]
+      bg-black
+      bg-opacity-50
+      flex
+      items-center
+      justify-center
+    "
+  >
+    <div class="flex">
+      <div>
+        <iframe
+          width="1143"
+          height="603"
+          src="https://www.youtube.com/embed/8ZKq0r-g87M"
+          title="What's Todoist?"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div class="-ml-5 -mt-5">
+        <div 
+        
+        class="w-11 h-11 rounded-full flex justify-center items-center bg-white hover:opacity-80">
+            <span class="text-xl font-bold">X</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
