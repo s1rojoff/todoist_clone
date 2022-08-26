b8476439a4df8dfd1af60fa6aef25013e5ce13fd
shukhratov1ch <jorayevazik586@gmail.com>
2022-08-26 09:47:48 +0300

    feat:some changes

--- src/js/VueRouter.js
+++ src/js/VueRouter.js
@@ -2,6 +2,7 @@ import {useRouter} from 'vue-router'
 function main(){
     const router = useRouter()
     function toSection(name){
+        name=name.toLowerCase()
         router.push(`${name}`)
     }
 
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,13 +2,17 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../views/HomePage.vue";
 import HelperPage from '../views/HelperPage.vue'
 import AuthSignUp from '../views/AuthSignUp.vue'
-
+import featuresPage from '../views/featuresPage.vue'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/",
       component: HomePage
+    },
+    {
+      path: "/features",
+      component : featuresPage
     },
     {
         path:'/auth',
