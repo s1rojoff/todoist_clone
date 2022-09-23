04c2a629dea021a4524fa01d892b5b5856b2d2cc
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2022-09-23 11:48:30 +0300

    feat: inbox section created

---
+++ src/components/InboxPage/InboxBody.vue
@@ -0,0 +1,43 @@
+<script setup>
+    import {ref} from 'vue';
+    import bodyAddTask from '../TodayPage/bodyAddTask.vue'
+    import AddSection from './AddSection.vue'
+    import MainPageImg from './Svgs/MainPageImg.vue'
+    import SoroqSvg from '../TodayPage/Svgs/SoroqSvg.vue'
+    import InputTaskName from './InputTaskName.vue'
+    import ActiveAdd from '../MainComponents/ActiveAdd.vue'
+    import {useStore} from "../../store";
+    const store = useStore();
+    let taskSecName = ref('')
+</script>
+
+<template>
+    <bodyAddTask/>
+    <div v-show="store.inbox_taskName">
+        <AddSection @click="store.inbox_taskName = false"/>
+        <div class="flex justify-center">
+            <MainPageImg class="md:w-1/4"/>
+        </div>
+        <div class="text-center">
+            <p>All clear</p>
+            <p class="text-sm text-gray-500 md:mt-5">Looks like everything's organized in the right place.</p>
+            <div class="flex justify-center md:mt-36 cursor-pointer">
+                <div class="flex items-center">
+                    <SoroqSvg/>
+                    <u class="text-red-600 text-sm">How to best use the Inbox</u>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <InputTaskName v-model="store.inbox_tasksSecName"/>
+        <div class="flex items-center md:mt-1">
+            <ActiveAdd :class="[store.inbox_tasksSecName ? 'bg-red-600' : 'bg-red-200']" class="text-white">
+                Add section
+            </ActiveAdd>
+            <ActiveAdd class="hover:bg-gray-100 md:ml-1">
+                Cancel
+            </ActiveAdd>
+        </div>
+    </div>
+</template>
--- src/components/InboxPage/InboxMain.vue
+++ src/components/InboxPage/InboxMain.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+import InboxTitle from './InboxTitle.vue'
+import InboxBody from './InboxBody.vue'
 </script>
 
 <template>
-    <div class="">
+    <div class="md:pl-32 md:pr-36 md:mt-7">
+        <InboxTitle/>
+        <InboxBody/>
 
     </div>
 </template>
